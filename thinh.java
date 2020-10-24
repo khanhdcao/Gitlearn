0ab581fcfe2123164b5b4e94ec227878c7da49da
Khanh Cao <khanhcao96@gmail.com>
2020-10-24 09:01:05 +0700

    added java file

---
+++ thinh.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+class thinh {
+    static void main(String[] args) {
+        int a;
+        for (int i = 0; i < 100; ++i) {
+            System.out.println(a);
+        }
+    }
+}
